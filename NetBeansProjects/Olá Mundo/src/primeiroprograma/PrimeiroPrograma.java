8326ca35a237cd88b29adfe71473b3924cd86ec5
Ederson Araujo <siulloong@gmail.com>
2022-10-28 16:21:39 -0300

    atualização 28/10/22

--- NetBeansProjects/Olá Mundo/src/primeiroprograma/PrimeiroPrograma.java
+++ NetBeansProjects/Olá Mundo/src/primeiroprograma/PrimeiroPrograma.java
@@ -1,10 +1,9 @@
-
 package primeiroprograma;
 
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Ola Mundo !!!");
+        System.out.println("Ola mundo!!!!");
     }
-  
+    
 }
--- NetBeansProjects/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ NetBeansProjects/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -1,9 +1,10 @@
+
 package primeiroprograma;
 
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
-        System.out.println("Ola mundo!!!!");
+        System.out.println("Ola Mundo !!!");
     }
-    
+  
 }
