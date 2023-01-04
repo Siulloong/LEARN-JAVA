9d68df1a026634933d65934f6be140c415248fee
Ederson Araujo <siulloong@gmail.com>
2023-01-03 21:51:08 -0300

    comit learn java 03-01

--- NetBeansProjects/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ NetBeansProjects/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -19,6 +19,41 @@ public class OperadoresAritmeticos {
         int n2 = 5;
         float m = (n1 + n2)/2;
         System.out.println("A media é igual a " + m);
+        
+        int numero = 5;
+        int numero1 = 5;
+        numero++;
+        numero1--;
+        
+        System.out.println(numero);
+        System.out.println(numero1);
+        System.out.println(5+ ++numero);
+        
+        /*int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        Sustem.out.println(numero);
+        */
+        
+        /*int x = 4;
+        x += 2; // x = x + 2
+        System.out.println(x);
+        */ 
+        
+        /* flot v = 8.9;
+        int ar = (int) Math.ceil(v); // ceil arredonda pra cima
+        System.out.println(ar);
+        int ar = (int) Math.floor(v); // ceil arredonda pra baixo
+        System.out.println(ar);
+        int ar = (int) Math.round(v); // ceil arredonda conforme a aritmetica
+        System.out.println(ar);
+        */
+        
+        /*double ale = Math.ramdom()
+        int n = (int) (1 + ale * (100-1)); // randomiza de 1 a 100
+        System.out.println(n);
+        
+        */
     }
-    
+     
 }
